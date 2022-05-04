39367097faf47a004aa07ab92b7676b63e55947e
jingyibo <2389153702@qq.com>
2022-05-04 19:08:20 +0800

    初始化仓库

---
+++ src/main/java/com/fc/StudentScoreSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentScoreSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentScoreSystemApplication.class, args);
+    }
+
+}
